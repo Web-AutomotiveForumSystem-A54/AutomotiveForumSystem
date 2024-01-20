0b4c691aa144de6b94b31f60236266b560e151f0
bobistefx <traev50@gmail.com>
2024-01-20 12:18:58 +0200

    committing

--- AutomotiveForumSystem/Models/Dtos/PostQueryParameters.cs
+++ AutomotiveForumSystem/Models/Dtos/PostQueryParameters.cs
@@ -4,5 +4,7 @@
     {
         public string? Category { get; set; }
         public string? Title { get; set; }
+        public string? Tag { get; set; }
+        public string? SortBy { get; set; }
     }
 }
